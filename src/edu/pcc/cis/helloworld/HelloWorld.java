213946088fec973256fdd44ebe19438845c5be28
Jonathan Romero Miranda <jonathan.romeromiranda@pcc.edu>
2022-04-25 01:37:57 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,13 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * A hello world program.
+ *
+ * @author Jonathan RM
+ * @version 2022.04.25
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
